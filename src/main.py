471b626c5e68ce85d982ec802fec6260c3df0523
Mayke <maykao123321@gmail.com>
2019-11-19 13:43:54 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import tweepy
+import json
+
+
+def main():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+def get_credentials():
+    pass
