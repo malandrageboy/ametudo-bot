dada8c4cf84cf39cb1a9a0040abd604943714aee
Mayke <maykao123321@gmail.com>
2019-11-19 18:07:26 -0300

    terminado

--- src/main.py
+++ src/main.py
@@ -27,8 +27,8 @@ def bot(api: tweepy.API):
         now = datetime.now(pytz.timezone("America/Sao_Paulo"))
         if str(now.minute).endswith("30"):
             content = f"Amo {get_random_word()} 💜"
-            # api.update_status(content)
-            print("[*] Novo post: "+content)
+            api.update_status(content)
+            print(f"[**] Novo post: {content}")
             time.sleep(60 + 5)
 
 def main():
@@ -39,7 +39,20 @@ def main():
     api = tweepy.API(auth)
     th = threading.Thread(target=bot, args=(api,))
     th.start()
-    create_stream(api)
+    try:
+        create_stream(api)
+    except:
+        print("[!!] ERRO NO STREAM, REINICIANDO...")
+        main()
+
+def cu():
+    auth = tweepy.OAuthHandler(get_credentials()['consumer_key'], get_credentials()['consumer_secret'])
+    auth.set_access_token(get_credentials()['access_token'], get_credentials()['access_token_secret'])
+
+    api = tweepy.API(auth)
+    content = "*Amamos liame 💜"
+    api.update_status(content, 1196895932190789634, auto_populate_reply_metadata=True)
 
 if __name__ == "__main__":
-    main()
+    # main()
+    cu()
--- src/stream.py
+++ src/stream.py
@@ -1,11 +1,29 @@
 import tweepy
+import time
+import json
+
+def get_credentials():
+    t = open("config/credentials.json")
+    j = json.loads(t.read())
+    t.close()
+    return j
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status: tweepy.Status):
+        auth = tweepy.OAuthHandler(get_credentials()['consumer_key'], get_credentials()['consumer_secret'])
+        auth.set_access_token(get_credentials()['access_token'], get_credentials()['access_token_secret'])
+        self.api = tweepy.API(auth)
         if status.user.screen_name == "Fodase_Bot":
             if not status.text.startswith("RT") and not status.text.startswith("@"):
-                print("[**] Novo post: " + status.text)
-
+                if status.text.startswith("Foda-se"):
+                    ww = status.text.split(" ")[1:]
+                    word = ""
+                    for w in ww:
+                        word += w + " "
+                    content = f"*Amamos {word} 💜"
+                    self.api.update_status(content, status.id, auto_populate_reply_metadata=True)
+                    print(f"[**] Novo post: {content}")
+        
 def create_stream(api: tweepy.API):
     print("[--] Stream iniciado [--]")
     myStreamListener = MyStreamListener()
